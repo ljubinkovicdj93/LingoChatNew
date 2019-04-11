6d26244d3762d890228a290393143960d7efed48
Djordje Ljubinkovic <ljubinkovicdj93@gmail.com>
2019-04-11 13:56:47 +0200

    Adds Alamofire library for networking tasks.

--- LingoChat/Pods/Target Support Files/Pods-LingoChat/Pods-LingoChat-frameworks.sh
+++ LingoChat/Pods/Target Support Files/Pods-LingoChat/Pods-LingoChat-frameworks.sh
@@ -143,11 +143,13 @@ strip_invalid_archs() {
 
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
+  install_framework "${BUILT_PRODUCTS_DIR}/Alamofire/Alamofire.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/Bond/Bond.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/Differ/Differ.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/ReactiveKit/ReactiveKit.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
+  install_framework "${BUILT_PRODUCTS_DIR}/Alamofire/Alamofire.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/Bond/Bond.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/Differ/Differ.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/ReactiveKit/ReactiveKit.framework"
